a9cfb5067db1854a268500440674adfb2dcd268b
AnshumanCse <anshumantiwari.cse@gmail.com>
2023-07-25 15:23:21 +0530

    initial commit

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.get("/", (req, res) => {
     message: "Port runnig succesfully",
   });
 });
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    success: true,
+    message: "Testing succesfully",
+  });
+});
 
 const PORT = process.env.PORT || 8080;
 
